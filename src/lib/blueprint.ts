fd4aa43ea924ce45e615e5f46d394db5b4f24f2b
Pawel <pawelstepniak22@gmail.com>
2024-06-20 00:03:23 +0200

    extend blueprint interface

--- src/lib/blueprint.ts
+++ src/lib/blueprint.ts
@@ -2,6 +2,24 @@ import type { Openapi } from './openapi.js'
 
 export interface Blueprint {
   name: string
+  language: string
+  methodDescription: string 
+  versionInformation: string 
+  category: string // e.g. Devices, Access Codes, Locks, etc.
+  parameters: Array<{
+    name: string 
+    type: string 
+    optionality: 'optional' | 'required' 
+    description: string 
+  }>
+  returnValue: {
+    type: string 
+    description: string 
+  }
+  examples: Array<{
+    codeSnippet: string 
+    explanation: string 
+  }>
 }
 
 export interface TypesModule {
