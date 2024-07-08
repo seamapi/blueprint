2d492df9cb42d8a750efa02efd396652f8660cee
Pawel <pawelstepniak22@gmail.com>
2024-07-08 11:41:02 +0200

    fix blueprint test

--- test/blueprint.test.ts
+++ test/blueprint.test.ts
@@ -1,10 +1,16 @@
 import test from 'ava'
 
-import { createBlueprint } from '@seamapi/blueprint'
+import { createBlueprint, type TypesModule } from '@seamapi/blueprint'
+
+import type { OpenAPI } from 'lib/openapi.js'
 
 import * as types from './fixtures/types/index.js'
 
+const typesModule: TypesModule = {
+  openapi: types.openapi as unknown as OpenAPI,
+}
+
 test('createBlueprint', (t) => {
-  const blueprint = createBlueprint(types)
+  const blueprint = createBlueprint(typesModule)
   t.snapshot(blueprint, 'blueprint')
 })
