d65a6c133c1ae935f255533fc192e49fdb7abf9f
Pawel <pawelstepniak22@gmail.com>
2024-07-23 14:16:31 +0200

    remove expect errors from tests

--- test/blueprint.test.ts
+++ test/blueprint.test.ts
@@ -5,7 +5,6 @@ import { createBlueprint } from '@seamapi/blueprint'
 import * as types from './fixtures/types/index.js'
 
 test('createBlueprint', (t) => {
-  // @ts-expect-error Remove once the fixture is properly typed
   const blueprint = createBlueprint(types)
   t.snapshot(blueprint, 'blueprint')
 })
--- test/seam-blueprint.test.ts
+++ test/seam-blueprint.test.ts
@@ -4,7 +4,6 @@ import test from 'ava'
 import { createBlueprint } from '@seamapi/blueprint'
 
 test('createBlueprint', (t) => {
-  // @ts-expect-error Remove once the import is properly typed
   const blueprint = createBlueprint({ openapi })
   t.snapshot(blueprint, 'blueprint')
 })
