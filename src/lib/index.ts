1f8687984d931c6f448d726a4081e2e7c3a6ecfe
Pawel <pawelstepniak22@gmail.com>
2024-07-23 12:41:29 +0200

    export subtypes from index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,13 @@
 export {
   type Blueprint,
   createBlueprint,
+  type Endpoint,
+  type Namespace,
+  type Parameter,
+  type Property,
+  type Request,
+  type Resource,
+  type Response,
+  type Route,
   type TypesModule,
 } from './blueprint.js'
