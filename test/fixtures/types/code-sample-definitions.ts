f8e31defa6085110b572658ba3fdaa3bb420c32c
Andrii Balitskyi <10balian10@gmail.com>
2024-08-05 17:36:03 +0200

    Update sample def and test snapshot

--- test/fixtures/types/code-sample-definitions.ts
+++ test/fixtures/types/code-sample-definitions.ts
@@ -17,4 +17,22 @@ export default [
       },
     },
   },
+  {
+    title: 'List foos',
+    description: 'This is the way to list foos',
+    request: {
+      path: '/foos/list',
+      parameters: {},
+    },
+    response: {
+      body: {
+        foos: [
+          {
+            foo_id: '8d7e0b3a-b889-49a7-9164-4b71a0506a33',
+            name: 'Best foo',
+          },
+        ],
+      },
+    },
+  },
 ]
