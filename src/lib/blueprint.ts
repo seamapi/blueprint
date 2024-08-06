f69e5f6975dec19393308655ef0ccab69ccc408d
Andrii Balitskyi <10balian10@gmail.com>
2024-08-06 17:29:04 +0200

    Correctly group endpoints by route

--- src/lib/blueprint.ts
+++ src/lib/blueprint.ts
@@ -204,9 +204,22 @@ const createRoutes = (
   targetPath: string,
   context: Context,
 ): Route[] => {
-  return Object.entries(paths)
-    .filter(([path]) => isFakeData || path === targetPath)
-    .map(([path, pathItem]) => createRoute(path, pathItem, context))
+  const routeMap = new Map<string, Route>()
+
+  Object.entries(paths)
+    .filter(([path]) => isFakeData || path.startsWith(targetPath))
+    .forEach(([path, pathItem]) => {
+      const route = createRoute(path, pathItem, context)
+
+      const existingRoute = routeMap.get(route.path)
+      if (existingRoute != null) {
+        existingRoute.endpoints.push(...route.endpoints)
+      } else {
+        routeMap.set(route.path, route)
+      }
+    })
+
+  return Array.from(routeMap.values())
 }
 
 const createRoute = (
