7e676977b04688914bd4098aa076c14c29d1457e
Andrii Balitskyi <10balian10@gmail.com>
2024-08-08 13:24:07 +0200

    Remove test/fixtures/types/route-schemas.ts

--- test/fixtures/types/index.ts
+++ test/fixtures/types/index.ts
@@ -5,5 +5,4 @@ export { schemas }
 export { default as codeSampleDefinitions } from './code-sample-definitions.js'
 export * from './model-types.js'
 export { default as openapi } from './openapi.js'
-export * from './route-schemas.js'
 export * from './route-types.js'
