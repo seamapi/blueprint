371be7c7704b89a73bf1fe95b05ec4f069971562
Andrii Balitskyi <10balian10@gmail.com>
2024-09-05 12:52:01 +0200

    Add number, object and array fixture props to test code samples, update snapshots

--- test/fixtures/types/schemas.ts
+++ test/fixtures/types/schemas.ts
@@ -1,9 +1,12 @@
-import { z } from 'zod'
+import { array, z } from 'zod'
 
 export const foo = z.object({
   foo_id: z.string().uuid(),
   name: z.string(),
   deprecated_prop: z.string().optional(),
   undocumented_prop: z.string().optional(),
-  nullable_property: z.string().optional().nullable(),
+  nullable_prop: z.string().optional().nullable(),
+  number_prop: z.number().optional(),
+  object_prop: z.record(z.string(), z.any()).optional(),
+  array_prop: array(z.string()).optional(),
 })
