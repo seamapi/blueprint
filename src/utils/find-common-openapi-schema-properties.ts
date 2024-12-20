97b8de897c6eb44308dcde414201241688457031
Andrii Balitskyi <10balian10@gmail.com>
2024-12-20 17:45:07 +0100

    Refactor findCommonOpenapiSchemaProperties using functional approach

--- src/utils/find-common-openapi-schema-properties.ts
+++ src/utils/find-common-openapi-schema-properties.ts
@@ -3,31 +3,20 @@ import type { OpenapiSchema } from 'lib/openapi.js'
 export function findCommonOpenapiSchemaProperties(
   schemas: OpenapiSchema[],
 ): Record<string, OpenapiSchema> {
-  if (schemas.length === 0 || schemas[0]?.properties == null) {
-    return {}
-  }
-
   const firstSchema = schemas[0]
-  if (firstSchema.properties == null) {
+  if (schemas.length === 0 || !firstSchema?.properties) {
     return {}
   }
 
-  const firstSchemaPropKeys = Object.keys(firstSchema.properties)
-
-  const commonPropKeys = firstSchemaPropKeys.filter((propKey) =>
-    schemas.every((schema) =>
+  return Object.entries(firstSchema.properties).reduce<
+    Record<string, OpenapiSchema>
+  >((commonProps, [propKey, propValue]) => {
+    const isPropInAllSchemas = schemas.every((schema) =>
       Object.keys(schema.properties ?? {}).includes(propKey),
-    ),
-  )
+    )
 
-  return commonPropKeys.reduce<Record<string, OpenapiSchema>>(
-    (result, propKey) => {
-      const propValue = firstSchema.properties?.[propKey]
-      if (propValue != null) {
-        result[propKey] = propValue
-      }
-      return result
-    },
-    {},
-  )
+    return isPropInAllSchemas
+      ? { ...commonProps, [propKey]: propValue }
+      : commonProps
+  }, {})
 }
