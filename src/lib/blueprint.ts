4fb28d19f9fbde5825929e563de27afecc4f49ec
Evan Sosenko <evan@getseam.com>
2025-06-27 15:30:49 -0700

    fix: Throw error on missing POST method

--- src/lib/blueprint.ts
+++ src/lib/blueprint.ts
@@ -677,10 +677,7 @@ const createEndpoint = async (
 
   const validOperation = validOperations.find(([m]) => m === 'post')
   if (validOperation == null) {
-    // eslint-disable-next-line no-console
-    console.warn(`No valid post operation found for ${path}`)
-
-    return null
+    throw new Error(`No valid post operation found for ${path}`)
   }
 
   const [_, operation] = validOperation
